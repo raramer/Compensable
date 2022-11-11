f97b188e669b0b1169a428f03a606abf8a965ada
raramer <>
2022-11-11 15:12:28 -0500

    change ForeachAsync to use TItem instead of T fix whitespace in ExecuteAsync

--- Compensable/AsyncCompensator/AsyncCompensator.ExecuteAsync.cs
+++ Compensable/AsyncCompensator/AsyncCompensator.ExecuteAsync.cs
@@ -5,42 +5,42 @@ namespace Compensable
 {
     partial class AsyncCompensator
     {
-		private async Task ExecuteAsync(Action validation, Func<Task> execution)
-		{
-			VerifyCanExecute();
+        private async Task ExecuteAsync(Action validation, Func<Task> execution)
+        {
+            VerifyCanExecute();
 
-			try
-			{
-				validation?.Invoke();
+            try
+            {
+                validation?.Invoke();
 
-				await _executionLock.WaitAsync(_cancellationToken).ConfigureAwait(false);
+                await _executionLock.WaitAsync(_cancellationToken).ConfigureAwait(false);
 
-				try
-				{
-					VerifyCanExecute();
+                try
+                {
+                    VerifyCanExecute();
 
-					await execution().ConfigureAwait(false);
-				}
-				catch
-				{
-					await SetStatusAsync(CompensatorStatus.FailedToExecute).ConfigureAwait(false);
-					throw;
-				}
-				finally
-				{
-					_executionLock.Release();
-				}
-			}
-			catch (Exception whileExecuting)
-			{
-				await CompensateAsync(whileExecuting).ConfigureAwait(false);
-				throw;
-			}
-		}
+                    await execution().ConfigureAwait(false);
+                }
+                catch
+                {
+                    await SetStatusAsync(CompensatorStatus.FailedToExecute).ConfigureAwait(false);
+                    throw;
+                }
+                finally
+                {
+                    _executionLock.Release();
+                }
+            }
+            catch (Exception whileExecuting)
+            {
+                await CompensateAsync(whileExecuting).ConfigureAwait(false);
+                throw;
+            }
+        }
 
-		#region Execution Overloads
-		private async Task ExecuteAsync(Action execution)
-			=> await ExecuteAsync(default(Action), execution.Awaitable()).ConfigureAwait(false);
+        #region Execution Overloads
+        private async Task ExecuteAsync(Action execution)
+            => await ExecuteAsync(default(Action), execution.Awaitable()).ConfigureAwait(false);
         #endregion
 
         #region Execution -> TResult Overloads
@@ -50,19 +50,19 @@ namespace Compensable
 
         #region Validation + Execution Overloads
         private async Task ExecuteAsync(Action validation, Action execution)
-			=> await ExecuteAsync(validation, execution.Awaitable()).ConfigureAwait(false);
-		#endregion
+            => await ExecuteAsync(validation, execution.Awaitable()).ConfigureAwait(false);
+        #endregion
 
-		#region Validation + Execution -> TResult Overloads
-		private async Task<TResult> ExecuteAsync<TResult>(Action validation, Func<Task<TResult>> execution)
-		{
-			var result = default(TResult);
-			await ExecuteAsync(validation, async () => 
-			{ 
-				result = await execution().ConfigureAwait(false); 
-			}).ConfigureAwait(false);
-			return result;
-		}
-		#endregion
-	}
+        #region Validation + Execution -> TResult Overloads
+        private async Task<TResult> ExecuteAsync<TResult>(Action validation, Func<Task<TResult>> execution)
+        {
+            var result = default(TResult);
+            await ExecuteAsync(validation, async () => 
+            { 
+                result = await execution().ConfigureAwait(false); 
+            }).ConfigureAwait(false);
+            return result;
+        }
+        #endregion
+    }
 }
